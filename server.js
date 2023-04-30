1930dc098e037016b51205e7cce8964268f5520e
Luis Pulido D <luis.pulido.d@bairesdev.com>
2023-04-30 00:24:45 -0700

    added filter for images

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const IMAGES_PER_PAGE = 15;
+const IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.gif', '.webp'];
+
+function filterImages(files) {
+  return files.filter(file => IMAGE_EXTENSIONS.includes(path.extname(file).toLowerCase()));
+}
 
 app.use(express.static('public'));
 
@@ -15,12 +20,13 @@ app.get('/api/images/:page/:limit', async (req, res) => {
   try {
     const imageDir = './images';
     const files = fs.readdirSync(imageDir);
-    const totalPages = Math.ceil(files.length / limit);
+    const filteredFiles = filterImages(files);
+    const totalPages = Math.ceil(filteredFiles.length / limit);
 
     const startIndex = (page - 1) * limit;
-    const endIndex = Math.min(page * limit, files.length);
+    const endIndex = Math.min(page * limit, filteredFiles.length);
 
-    const images = files.slice(startIndex, endIndex).map(file => ({
+    const images = filteredFiles.slice(startIndex, endIndex).map(file => ({
       src: `images/${file}`,
       alt: path.basename(file, path.extname(file)),
     }));
